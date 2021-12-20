cfd50d9c84eec4ea087f3e3ea808faad83dec504
Muhammad Rendusara <hadyanrendusara@yahoo.com>
2021-12-20 12:58:38 -0600

    commit tree 40bffd6fb09f18810a2275c3ca651a228b630509

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
-# your code goes here
+require 'pry'
+
+class Person
+    
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name=name
+        @bank_account=25
+        @happiness=8
+        @hygiene=8
+    end
+    def happiness
+        @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness < 0
+        @happiness
+    end
+    def hygiene
+        @hygiene = 10 if @hygiene > 10
+        @hygiene = 0 if @hygiene < 0
+        @hygiene
+    end    
+end
+binding.pry
